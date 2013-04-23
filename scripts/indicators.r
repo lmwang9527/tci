481e137f7ac2143e96367b72f302ffb40e50324d
lmw <lmwang@gmail.com>
2013-04-22 17:47:26 -0700

    added housing_units dataset for testing

--- scripts/indicators.r
+++ scripts/indicators.r
@@ -1,34 +1,42 @@
+#define utility functions for data quality indicators
+
 count.na <- function(x) sum(is.na(x))
 all.na <- function(x, y) max(count.na(x), count.na(y))==length(y)
 rmse <- function(x=x, y=y) sqrt(mean((x-y)^2))
-r2 <- function(x=x, y=y, ...) {if(!all.na(x, y)) summary(lm(y~x,...))$r.squared else 0.0}
-intercept <- function(x=x, y=y, ...) {if(!all.na(x,y)) coef(lm(y~x,...))["(Intercept)"] else 0.0}
-slope <- function(x=x, y=y, ...) {if(!all.na(x,y)) coef(lm(y~x,...))["x"] else 0.0}
+r2 <- function(x=x, y=y, ...) {if(!all.na(x, y)) summary(lm(y~x,...))$r.squared else NA}
+intercept <- function(x=x, y=y, ...) {if(!all.na(x,y)) coef(lm(y~x,...))["(Intercept)"] else NA}
+slope <- function(x=x, y=y, ...) {if(!all.na(x,y)) coef(lm(y~x,...))["x"] else NA}
 
-setwd('/Users/lmwang/Dropbox/smartdata/04152013')
-load("ALL.Rda")
-head(ALL, 10)
+#install.packages("plyr")
+#install.packages("data.table")
 
-install.packages("plyr")
-install.packages("data.table")
+## test and sample application
+require(plyr)
+require(data.table)
 
-require("plyr")
-library(data.table)
-all.dt <- data.table(ALL, key = "Year")
-head(all.dt, 10)
+# test data
+#setwd('/Users/lmwang/Dropbox/smartdata/04152013')
+#load("ALL.Rda")
+#head(ALL, 10)
 
+units <- read.table("housing_units.csv", sep=",", header=T)
+units.dt <- data.table(units, key = "Year")
+head(units.dt, 10)
+
+## compute DQI with pluggable indicators
 #solution 1
-#ddply(all.dt[, ':='(x=RLIS_MFH, y=ACS_MFH)], .(Year), 
+#ddply(units.dt[, ':='(x=RLIS_MFH, y=ACS_MFH)], .(Year), 
 #      summarise, rmse=sqrt(mean((x-y)^2)))      
 
 #solution 2
-#all.dt[, ':='(x=RLIS_MFH, y=ACS_MFH)]
-#all.dt[, rmse:=sqrt(mean((x-y)^2)), by="Year"]
-##all.dt[, lapply(.SD, function(x, y) sqrt(mean((x-y)^2))), by="Year"]
+#units.dt[, ':='(x=RLIS_MFH, y=ACS_MFH)]
+#units.dt[, rmse:=sqrt(mean((x-y)^2)), by="Year"]
+##units.dt[, lapply(.SD, function(x, y) sqrt(mean((x-y)^2))), by="Year"]
 
-ddply(all.dt, .(Year), summarise, 
+dqi <- ddply(units.dt, .(Year), summarise, 
       mfh.rmse=rmse(x=RLIS_MFH, y=ACS_MFH),
       mfh.r2=r2(x=RLIS_MFH, y=ACS_MFH),
       mfh.intercept=intercept(x=RLIS_MFH, y=ACS_MFH),
       mfh.slope=slope(x=RLIS_MFH, y=ACS_MFH)
       )
+print(dqi)
